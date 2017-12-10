ebcfb9655a8b81a2d0b744ad6518ab2f998c66e4
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-10 03:09:05 -0600

    converting to react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>You did it!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
